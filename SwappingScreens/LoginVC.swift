62eb8cb120ed503d151da3b89bc0cc6d7442eba2
Matt Tripodi <mtripodi1027@gmail.com>
2017-05-09 17:54:49 -0400

    Created Segue and passed song title to third view controller

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Matt Tripodi on 5/8/17.
+//  Copyright © 2017 Matt Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+}
+
